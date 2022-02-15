8005a3402baaa5bed1cecef930ec33b91b51f0e0
Yacine <bouyakoubyacine1@gmail.com>
2022-02-15 14:52:09 +0100

    first commit

---
+++ loginPage.js
@@ -0,0 +1,151 @@
+const {
+  Builder,
+  By,
+  Key,
+  until,
+  WebElementCondition,
+} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+const assert = require('assert');
+
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
+(async function example() {
+  // const options = new chrome.Options().setUserPreferences (
+  //   'profile.default_content_settings.geolocation',
+  //   2
+  // );
+
+  const driver = await new Builder()
+    .forBrowser('chrome')
+    // .setChromeOptions(options)
+    .build();
+  try {
+    await driver.get('http://localhost:3000/');
+    // await driver.wait();
+    // await driver.manage().setTimeouts({ implicit: 10000 });
+
+    // wait for the page to load (center position)
+    await driver.sleep(1000000);
+
+    // wait until zoom button is ready
+    await driver.wait(
+      until.elementLocated(
+        By.xpath(
+          '//*[@id="root"]/div/div[1]/div/div[2]/div[1]/div/button[2]/span'
+        )
+      ),
+      10000
+    );
+
+    //click on zoom button
+    await driver
+      .findElement(
+        By.xpath(
+          '//*[@id="root"]/div/div[1]/div/div[2]/div[1]/div/button[2]/span'
+        )
+      )
+      .click();
+
+    //click a second time on the zoom button (to display an art on the map)
+    await driver
+      .findElement(
+        By.xpath(
+          '//*[@id="root"]/div/div[1]/div/div[2]/div[1]/div/button[2]/span'
+        )
+      )
+      .click();
+
+    //wait for the art to show on screen
+    await driver.wait(
+      until.elementLocated(
+        By.css(
+          '#root > div > div:nth-child(1) > div > div.overlays > div.mapboxgl-marker > svg'
+        )
+      ),
+      5000
+    );
+
+    // click on art
+    await driver
+      .findElement(
+        By.css(
+          '#root > div > div:nth-child(1) > div > div.overlays > div.mapboxgl-marker > svg'
+        )
+      )
+      .click();
+
+    // scroll inside the popup to show all the details of the art
+    await driver.executeScript(`
+        const popUp = document.querySelector("#root > div > div.popupCard2");
+        popUp.scrollTo(0, document.body.scrollHeight);
+        `);
+
+    // verify that the image is displayed
+    assert(
+      (await driver
+        .findElement(
+          By.css(
+            '#carouselExampleCaptions > div.carousel-inner.relative.w-full.overflow-hidden > div > div > img'
+          )
+        )
+        .isDisplayed()) === true,
+      'image of art is not displayed'
+    );
+
+    console.log('image displayed correctly');
+
+    assert(
+      (await driver
+        .findElement(
+          By.css('#root > div > div.popupCard2 > div.px-6.py-4 > div')
+        )
+        .isDisplayed()) === true,
+      'title of artwork is not displayed'
+    );
+
+    console.log('title is displayed correctly');
+
+    assert(
+      (await driver
+        .findElement(
+          By.css(
+            '#root > div > div.popupCard2 > div.px-6.py-4 > blockquote > p'
+          )
+        )
+        .isDisplayed()) === true,
+      'description of artwork is not displayed'
+    );
+
+    console.log('description is displayed correctly !');
+
+    assert(
+      (await driver
+        .findElement(
+          By.css(
+            '#root > div > div.popupCard2 > div.px-6.py-4 > figcaption > div.text-sky-500.dark:text-sky-400.text-lg.mt-3'
+          )
+        )
+        .isDisplayed()) === true,
+      'artist name is not displayed'
+    );
+
+    console.log('artist name is displayed correctly !');
+
+    assert(
+      (await driver
+        .findElement(
+          By.css(
+            '#root > div > div.popupCard2 > div.px-6.py-4 > figcaption > div.text-slate-700.dark:text-slate-500'
+          )
+        )
+        .isDisplayed()) === true,
+      'Location is not displayed'
+    );
+
+    console.log('location is displayed correctly !');
+  } finally {
+    await driver.quit();
+  }
+})();
---
+++ userLogin.js
@@ -0,0 +1,79 @@
+const {
+  Builder,
+  By,
+  Key,
+  until,
+  WebElementCondition,
+} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+const assert = require('assert');
+require('dotenv').config();
+
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
+(async function example() {
+  const driver = await new Builder().forBrowser('chrome').build();
+  try {
+    // go to app
+    await driver.get('http://localhost:3000/');
+
+    // wait for the page to load (center position)
+    await driver.sleep(5000);
+
+    const loginButton = await driver.wait(
+      until.elementLocated(By.css('#login > a'))
+    );
+    loginButton.click();
+
+    const email = await driver.wait(until.elementLocated(By.css('#email')));
+    email.sendKeys('abcde');
+
+    await driver.wait(until.elementLocated(By.css('#email-helper-text')));
+
+    const wrongEmail = await driver
+      .findElement(By.css('#email-helper-text'))
+      .getText();
+    assert(
+      wrongEmail === 'Email invalide',
+      'wrong email message not displayed properly ! '
+    );
+
+    console.log('wrong email message displayed correctly !');
+
+    email.clear();
+    email.sendKeys('abcde');
+    driver.sleep(5000);
+    email.sendKeys('mail.com');
+
+    const password = await driver.wait(
+      until.elementLocated(By.css('#password'))
+    );
+    password.sendKeys(process.env.LESS_EIGHT_PASSWORD);
+
+    const l8c = await driver.wait(
+      until.elementLocated(By.css('#password-helper-text'))
+    );
+    assert(
+      l8c.getText() === 'Minimum 8 caractères',
+      'wrong password less then 8, not displayed !'
+    );
+
+    console.log('wrong password less then 8 displayed !');
+
+    password.clear();
+    password.sendKeys(process.env.NO_UPPER_PASSWORD);
+    const nup = await driver.wait(
+      until.elementLocated(By.css('#password-helper-text'))
+    );
+    assert(
+      nup.getText() === 'Doit contenir au moins un caractère en majuscule',
+      'wrong password no upper case letter, not displayed !'
+    );
+
+    console.log('wrong password no upper case letter displayed !');
+  } finally {
+    await driver.sleep(10000);
+    await driver.quit();
+  }
+})();
