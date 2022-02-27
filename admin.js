66b9245fa8aa5866a40e5ffbe12ae6fb324f9926
Yacine <bouyakoubyacine1@gmail.com>
2022-02-27 19:14:35 +0100

    admin-add-artwork/progress

--- admin.js
+++ admin.js
@@ -98,40 +98,46 @@ chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
     const artist = await driver.findElement(By.css('#artist'));
     await artist.clear();
-    await artist.sendKeys(faker.name.findName());
+    await artist.sendKeys(faker.name.firstName());
 
-    const description = await driver.findElement(By.css('#description'));
+    // const descriptionTooLong = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel eros interdum, rhoncus erat eget, blandit purus. Fusce faucibus, mauris nec ultrices mattis, tellus ligula suscipit nunc, et scelerisque augue turpis et est. Suspendisse lacus leo, cursus cursus ullamcorper eu, ornare sit amet enim. Mauris vulputate semper ipsum quis vehicula. Nunc fermentum id augue ac laoreet. Maecenas vulputate magna vel efficitur pellentesque. Curabitur pulvinar ante felis, a efficitur ligula porta ut. Cras ornare, tellus non dignissim laoreet, augue nunc vehicula eros, sit amet convallis dui mauris sed ipsum. Aliquam viverra, nisi at fermentum imperdiet, orci purus gravida augue, ac elementum dui felis eu lectus. Pellentesque augue diam, elementum commodo dolor eu, euismod luctus est.
 
-    await description.clear();
-    await description.sendKeys('a');
+    //   Phasellus eget nisi ac tellus sollicitudin sollicitudin. Vestibulum quis nibh commodo, pulvinar erat convallis, facilisis magna. Mauris vulputate ultrices enim, ac venenatis urna vestibulum luctus. Praesent erat lacus, vulputate at risus in, gravida feugiat nibh. Nulla eu augue vel sapien finibus malesuada. Aenean eget enim vel odio faucibus egestas ac vitae ligula. Proin mattis a orci nec auctor. Aliquam erat volutpat. Fusce posuere orci enim, vel mattis velit maximus sit amet. In hac habitasse platea dictumst. Vivamus sem neque, feugiat ullamcorper velit a, imperdiet tempus enim. Pellentesque tincidunt porta enim, vestibulum pellentesque ligula semper et. Morbi molestie, nisi commodo lobortis efficitur, lectus nisl rhoncus ante, nec mattis mi purus sit amet magna. Morbi faucibus molestie tincidunt.
 
-    const descriptionError = await driver.findElement(
-      By.css('#description-helper-text')
-    );
+    //   Integer non mollis ligula, ac fermentum massa. Fusce ac volutpat ex, dignissim tincidunt dui. Proin facilisis erat mi, et dictum ligula dignissim a. Sed euismod.`;
 
-    assert(
-      (await descriptionError.isDisplayed()) === true &&
-        (await descriptionError.getText()) ===
-          'Description doit contenir entre 2 et 250 caractéres',
-      'FAILED : message description error not displayed !'
-    );
-    console.log('PASSED : message description error displayed !');
+    // const description = await driver.findElement(By.css('#description'));
 
-    await description.clear();
-    await description.sendKeys(faker.lorem.sentences(25, ' '));
-    assert(
-      (await descriptionError.isDisplayed()) === true &&
-        (await descriptionError.getText()) ===
-          'Description doit contenir entre 2 et 250 caractéres',
-      'FAILED : message description error not displayed !'
-    );
-    console.log('PASSED : message description error displayed !');
+    // await description.clear();
+    // await description.sendKeys('a');
 
-    await description.clear();
-    await description.sendKeys(faker.lorem.sentences(4, ' '));
+    // const descriptionError = await driver.findElement(
+    //   By.css('#description-helper-text')
+    // );
 
-    const addImage = await driver.findElement(By.css('#icon-button-file1'));
-    await addImage.sendKeys(process.env.IMAGE_PATH);
+    // assert(
+    //   (await descriptionError.isDisplayed()) === true &&
+    //     (await descriptionError.getText()) ===
+    //       'Description doit contenir entre 2 et 250 caractéres',
+    //   'FAILED : message description error not displayed !'
+    // );
+    // console.log('PASSED : message description error displayed !');
+
+    // await description.clear();
+    // await description.sendKeys(faker.lorem.sentences(25, ' '));
+    // assert(
+    //   (await descriptionError.isDisplayed()) === true &&
+    //     (await descriptionError.getText()) ===
+    //       'Description doit contenir entre 2 et 250 caractéres',
+    //   'FAILED : message description error not displayed !'
+    // );
+    // console.log('PASSED : message description error displayed !');
+
+    // await description.clear();
+    // await description.sendKeys(faker.lorem.sentences(4, ' '));
+
+    // const addImage = await driver.findElement(By.css('#icon-button-file1'));
+    // await addImage.sendKeys(process.env.IMAGE_PATH);
 
     //click on change position
     await driver
@@ -139,12 +145,12 @@ chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
       .click();
 
     // const windowSize = await driver.manage().window().getSize();
-    const mapOverlay = await driver.findElement(
+    let mapOverlay = await driver.findElement(
       By.css(
         'body > div.MuiModal-root.blure.css-bvyw50 > div.MuiBox-root.css-1p6yjsm > div > div > div.overlays'
       )
     );
-    const windowSize = await mapOverlay.getRect();
+    let windowSize = await mapOverlay.getRect();
     const actions = driver.actions({ async: true });
     await actions
       .move({
@@ -154,6 +160,8 @@ chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
       // .pause(3000)
       .perform();
     await actions.click().perform();
+
+    await driver.findElement(By.css('#addBtn')).click();
 
     // await driver.findElement(By.css('#mui-2')).click();
 
@@ -200,6 +208,49 @@ chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
     // console.log(
     //   'PASSED : when clicked the add button redirected to add page for admin'
     // );
+    await driver.findElement(By.css('#retBtn')).click();
+    assert(
+      (await driver.getCurrentUrl()) === 'http://localhost:3000/map/admin',
+      'FAILED : should return to main page'
+    );
+    console.log('PASSED : returned to main page');
+
+    await driver.findElement(By.css('#addBtn')).click();
+    assert(
+      (await driver.getCurrentUrl()) === 'http://localhost:3000/form/admin',
+      'FAILED : redirected to add art form !'
+    );
+    console.log('PASSED : redirected to add art form !');
+
+    const titreAdd = await driver.findElement(By.css('#title'));
+    await titreAdd.sendKeys(process.env.TITLE);
+    const artistAdd = await driver.findElement(By.css('#artist'));
+    await artistAdd.sendKeys(process.env.ARTIST);
+    const descriptionAdd = await driver.findElement(By.css('#description'));
+    await descriptionAdd.sendKeys(process.env.DESCRIPTION);
+    const imageAdd = await driver.findElement(By.css('#icon-button-file'));
+    await imageAdd.sendKeys(process.env.IMAGE_PATH);
+    await driver
+      .findElement(By.css('#root > div > main > div > form > div > button'))
+      .click();
+
+    // const windowSize = await driver.manage().window().getSize();
+    mapOverlay = await driver.findElement(
+      By.css(
+        'body > div.MuiModal-root.blure.css-bvyw50 > div.MuiBox-root.css-1p6yjsm > div > div > div.overlays'
+      )
+    );
+    windowSize = await mapOverlay.getRect();
+    await actions
+      .move({
+        x: parseInt(windowSize.width / 2),
+        y: parseInt(windowSize.height / 2),
+      })
+      // .pause(3000)
+      .perform();
+    await actions.click().perform();
+
+    await driver.findElement(By.css('#addBtn')).click();
   } finally {
     await driver.sleep(8000);
     await driver.quit();
